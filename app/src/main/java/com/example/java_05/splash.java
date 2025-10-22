ab07362c64c7abce640b67b155eaf999867bcc69
madee@CM_Tech <madeel466466@gmail.com>
2025-10-22 23:38:29 +0500

    first commit

---
+++ app/src/main/java/com/example/java_05/splash.java
@@ -0,0 +1,34 @@
+package com.example.java_05;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash extends AppCompatActivity {
+      int splash_time_out = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(splash.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, splash_time_out);
+
+
+
+
+
+
+
+    }
+}
